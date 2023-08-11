cc29383d7cf79a5c48dfa0fe6e10cdf5f3a27373
n1o2h <aythmadnhylt@gmail.com>
2023-08-11 14:29:17 -0700

    done

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+  * main - prints all possible combinations of two two-digit numbers
+  * Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("enter a number :");
+scanf(" % " d, &n);
+if (n == 0)
+{
+printf("the number %d is zero", n);
+}
+else
+{
+if (n > 0)
+{
+printf("the number  %d is positive", n);
+}
+else
+{
+printf("the number %d is negative ", n);
+}
+return (0);
+}
