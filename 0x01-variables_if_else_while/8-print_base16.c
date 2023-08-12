4be9dd33c218e1ceb7032b643b1f7169efe99699
n1o2h <aythmadnhylt@gmail.com>
2023-08-11 18:27:49 -0700

    done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  * main - prints all numbers in hexadecimal
+  * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i = 48; /* 48: decimal repo of 0*/
+
+
+while (i <= 102) /* 102: decimal repo of f*/
+{
+putchar(i);
+/* after 9 we jump till 96; */
+if  (i == 57) /* 57: decimal repo of 9*/
+i += 39;
+i++;
+}
+return (0);
+}
