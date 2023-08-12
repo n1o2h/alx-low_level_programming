67df3620a7ef878b0c27a890470394d3b7d448dd
n1o2h <aythmadnhylt@gmail.com>
2023-08-11 17:20:25 -0700

    done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - prints all alphabet with out q and e
+  * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char ch = 'a';
+while (ch != 'q' && ch != 'e')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
