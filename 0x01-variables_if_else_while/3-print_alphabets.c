4fd4011b0bb49cbbb02453ad9afa3f9985b0c72d
n1o2h <aythmadnhylt@gmail.com>
2023-08-11 17:03:31 -0700

    done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+  * main - prints all alphabet letters and  in uppercas
+  * Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch = 'a';
+char  Ch = 'A';
+/* prints a - z*/
+while (ch <= 'z')
+{
+	putchar(ch);
+ch++;
+}
+
+/* prints A - Z*/
+
+while (Ch <= 'Z')
+{
+putchar(Ch);
+Ch++;
+}
+
+putchar('\n');
+
+}
