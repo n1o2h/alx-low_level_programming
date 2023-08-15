230eac004371019395df59be0ea6a0f8eecc4ad5
n1o2h <aythmadnhylt@gmail.com>
2023-08-15 14:58:16 -0700

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "mian.h"
+
+/**
+ *_abs - function that coputes thz absolute
+ *value of an integer
+ *@n: takes in integer type input for function
+ *Return: Always 0 (success)
+*/
+
+int _abs(int n)
+{
+if (n < 0)
+n = (-1) * n;
+return (0);
+}
