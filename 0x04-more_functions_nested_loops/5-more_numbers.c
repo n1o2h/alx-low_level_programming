40a10a59c3b0e97d05af99027e43a3a64e014f24
n1o2h <aythmadnhylt@gmail.com>
2023-08-17 14:14:34 -0700

    done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,52 @@
 #include "main.h"
 
 /**
- *more_numbers - print 0 - 14 ten times and you
- *can only use _putchar three times
- *
- *Return: Always 0 (Success)
- *
+ * print_one_digit - prints one digit
+ * @n: the number to be printed
  */
+
+void print_one_digit(int n)
+{
+	_putchar(n % 10 + 48);
+}
+
+/**
+ * print_numbers - prints numbers from 0 to 14
+ * description: prints numbers starting from 0 to 14
+ */
+
+void print_numbers(void)
+{
+	int number;
+
+	number = 0;
+	while (number <= 14)
+	{
+		if (number >= 10)
+		{
+			_putchar(number / 10 + 48);
+			print_one_digit(number);
+		}
+		else
+			print_one_digit(number);
+		number++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * more_numbers - prints numbers
+ * description: prints numbers starting from 0 to 14 10 timess
+ */
+
 void more_numbers(void)
 {
-int num, row, count;
-for (row = 1; row <= 10; row++)
-{
-for (count = 0; count <= 14; count++)
-{
-num = count;
-if (count >= 9)
-_putchar(1 + 48);
-num = count % 10;
-}
-_putchar(num + 48);
-}
-_putchar('\n');
+	int i;
+
+	i = 1;
+	while (i <= 10)
+	{
+		print_numbers();
+		i++;
+	}
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
+#ifndef MAIN
+#define MAIN
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -10,4 +7,9 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 #endif
