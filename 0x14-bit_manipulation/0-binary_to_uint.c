03855193e01c8e7518d0c9480bc69c5675ae2872
n1o2h <aythmadnhylt@gmail.cm>
2023-10-27 14:30:22 -0700

    done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,6 +13,7 @@ int is_b(const char *b)
 		if (*b != '0' && *b != '1')
 			return (0);
 		b++;
+
 		if (!*b)
 			return (1);
 	}
@@ -31,6 +32,7 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
+
 	if (is_b(b))
 	{
 		for (i = 0; b[i]; i++)
